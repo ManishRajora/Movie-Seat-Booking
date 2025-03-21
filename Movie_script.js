d95cf69f1fb82eda26e2128cbbc1010b621dd1e6
Manish chaudhary <manish.22b1531078@abes.ac.in>
2025-03-21 10:53:57 +0530

    adding javascript

---
+++ Movie_script.js
@@ -0,0 +1,56 @@
+const movie = document.querySelector('.movie');
+const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const confirm_btn = document.querySelector('.confirm_btn');
+const payment_btn = document.querySelector('.payment_btn');
+const amount = document.getElementById('amount');
+const close_btn = document.querySelector('.close');
+const conatiner = document.querySelector('.container');
+const payment_window = document.querySelector('.payment_window');
+const success_window = document.querySelector('.payment_success');
+const payment_form = document.querySelector('.payment_form');
+
+let ticket_price = parseInt(movie.value);
+let seat_count = 0;
+let total_price = 0;
+
+// function to update count and total price
+function update_count(){
+    const selected_seats = document.querySelectorAll('.row .seat.selected');
+    seat_count = parseInt(selected_seats.length);
+    total_price = seat_count * ticket_price;
+    count.innerText = seat_count;
+    total.innerText = total_price;
+}
+
+// change the price according to selected movie
+movie.addEventListener('change', function(event){
+    ticket_price = parseInt(event.target.value);
+    update_count();
+});
+
+// to toggle the selected seats
+conatiner.addEventListener('click', function(event){
+    if(event.target.classList.contains('seat') && !event.target.classList.contains('occupied')){
+        event.target.classList.toggle('selected');
+        update_count();
+    }
+});
+
+// confirm btn to show payment window
+confirm_btn.addEventListener('click', function(){
+    if(seat_count > 0){
+        payment_window.style.display = 'block';
+        amount.value = total_price;
+    }
+});
+
+payment_btn.addEventListener('click', function(){
+    payment_form.style.display = 'none';
+    success_window.style.display = 'block';
+});
+
+close_btn.addEventListener('click', function(){
+    location.reload();  
+});
